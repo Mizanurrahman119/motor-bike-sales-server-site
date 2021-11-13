6081bf0d078d28e652ec0c9a1ed2999d3742df60
Mizanurrahman119 <niloy719119@gmail.com>
2021-11-13 12:17:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const {MongoClient} = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const cors =  require('cors');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle ware 
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oqk0s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try{
+        await client.connect();
+        const database = client.db('motorBike');
+        const serviceCollection = database.collection('services');
+        const exploreCollection = database.collection('explores');
+
+        //get api 
+        app.get('/services', async (req, res) => {
+            const cursor =  serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        // get api
+        app.get('/explores', async (req, res) => {
+            const cursor =  exploreCollection.find({});
+            const explores = await cursor.toArray();
+            res.send(explores);
+        });
+        
+        //post api 
+        app.post('/explores', async(req, res) => {
+            const explores = req.body;
+            const result = await exploreCollection.insertOne(explores);
+            console.log(result);
+            res.json(result);
+        });
+
+        //delete api
+        app.delete('/explores/:id', async (req, res) => {
+            const id = req.params.id;
+            const query  = {_id:ObjectId(id)};
+            const result = await exploreCollection.deleteOne(query);
+            res.json(result);
+        })
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('running motor bike sales server');
+});
+
+app.listen(port, () => {
+    console.log('running motor bike sales server', port);
+})
